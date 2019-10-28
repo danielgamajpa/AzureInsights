68c1fc2638c2c5e178aa5e08bcae6ec812de4795
Daniel Gama <danielgamajpa@gmail.com>
2019-10-28 12:55:59 -0300

    commit inicial

---
+++ Insights/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Insights
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 27);
+            this.button1.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(231, 160);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ação 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(319, 27);
+            this.button2.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(231, 160);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Ação 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(585, 27);
+            this.button3.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(231, 160);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Ação 3 + Exception";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(843, 27);
+            this.button4.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(231, 160);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Ação 4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(15F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1130, 321);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Insights/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Insights
+{
+    public partial class Form1 : Form
+    {
+
+        private const string CHAVE_INSTRUMENTACAO = "CHAVE_INSTRUMENTACAO";
+
+        Microsoft.ApplicationInsights.TelemetryClient telemetria;
+
+        string SessaoUsuario;
+
+        Dictionary<string, string> propriedades;
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            SessaoUsuario = Guid.NewGuid().ToString();
+
+            telemetria = new Microsoft.ApplicationInsights.TelemetryClient();
+            telemetria.InstrumentationKey = CHAVE_INSTRUMENTACAO;
+            telemetria.Context.Session.Id = SessaoUsuario;
+
+            EnviarEventosTelemetria("Sistema Iniciado");
+
+            propriedades = new Dictionary<string, string>();
+            propriedades.Add("VersaoApp", Application.ProductVersion);
+            propriedades.Add("UF", "PB");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            EnviarEventosTelemetria("Botao 1 Clicado");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            EnviarEventosTelemetria("Botao 2 Clicado");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            EnviarEventosTelemetria("Botao 3 Clicado");
+
+            try
+            {
+                throw new Exception("Erro");
+            }
+            catch (Exception ex)
+            {
+                EnviarExceptionsTelemetria(ex);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            EnviarEventosTelemetria("Botao 4 Clicado");
+        }
+
+        //-------------------------------------
+        //-------------------------------------
+        //-------------------------------------
+        private void EnviarEventosTelemetria(string NomeEvento)
+        {
+            telemetria.TrackEvent(NomeEvento, propriedades);
+
+            telemetria.Flush();
+        }
+
+        private void EnviarExceptionsTelemetria(Exception ex)
+        {
+            telemetria.TrackException(ex, propriedades);
+
+            telemetria.Flush();
+        }
+    }
+}
